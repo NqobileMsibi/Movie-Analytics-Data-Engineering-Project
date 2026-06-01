caf2f3c97861204110f85937432b9e1f3195e39b
Nqobile Msibi <129752024+NqobileMsibi@users.noreply.github.com>
2026-06-01 19:49:56 +0200

    Refactor SQL queries for review analysis

--- SQL/Exploratory Analysis.sql
+++ SQL/Exploratory Analysis.sql
@@ -1,10 +1,15 @@
+-- How many reviews were done in California, NY and Texas?
+
 SELECT 
     dl.location,
     COUNT(f.review_count) AS reviews
 FROM "EDW".fact_movie_analytics f
-INNER JOIN "EDW".dim_location dl ON dl.id_dim_location = f.id_dim_location
-INNER JOIN "EDW".dim_devices  dd ON dd.id_dim_devices  = f.id_dim_devices
-INNER JOIN "EDW".dim_os       dos ON dos.id_dim_os     = f.id_dim_os
+INNER JOIN "EDW".dim_location dl 
+	ON dl.id_dim_location = f.id_dim_location
+INNER JOIN "EDW".dim_devices dd 
+	ON dd.id_dim_devices = f.id_dim_devices
+INNER JOIN "EDW".dim_os dos 
+	ON dos.id_dim_os = f.id_dim_os
 WHERE dl.location IN (
 	'California',
 	'New York',
@@ -12,15 +17,19 @@ WHERE dl.location IN (
 )
 GROUP BY dl.location;
 
+--How many reviews were done in California, NY, and Texas with an Apple device? And how many for each device type?
 
 SELECT 
     dl.location,
     dd.device,
     COUNT(f.review_count) AS Review
 FROM "EDW".fact_movie_analytics f
-INNER JOIN "EDW".dim_location dl ON dl.id_dim_location = f.id_dim_location
-INNER JOIN "EDW".dim_devices  dd ON dd.id_dim_devices  = f.id_dim_devices
-INNER JOIN "EDW".dim_os       dos ON dos.id_dim_os     = f.id_dim_os
+INNER JOIN "EDW".dim_location dl 
+	ON dl.id_dim_location = f.id_dim_location
+INNER JOIN "EDW".dim_devices dd 
+	ON dd.id_dim_devices = f.id_dim_devices
+INNER JOIN "EDW".dim_os dos 
+	ON dos.id_dim_os = f.id_dim_os
 WHERE os = 'Apple iOS' 
 AND dl.location IN (
 	'California',
@@ -30,15 +39,27 @@ AND dl.location IN (
 GROUP BY dl.location, dd.device
 ORDER BY dl.location, Review DESC;
 
+--What are the states with more and fewer reviews in 2021?
 
 SELECT 
     dl.location,
-	  dd.device,
-    dos.os,
-    to_char(SUM(f.amount_spent), 'R FM999,999,990.00') AS Total
+    COUNT(f.review_count) AS reviews
 FROM "EDW".fact_movie_analytics f
 INNER JOIN "EDW".dim_location dl ON dl.id_dim_location = f.id_dim_location
 INNER JOIN "EDW".dim_devices  dd ON dd.id_dim_devices  = f.id_dim_devices
 INNER JOIN "EDW".dim_os       dos ON dos.id_dim_os     = f.id_dim_os
-GROUP BY dl.location, dd.device, dos.os
-ORDER BY Total;
+GROUP BY dl.location
+ORDER BY reviews DESC
+LIMIT 10;
+
+
+SELECT 
+    dl.location,
+    COUNT(f.review_count) AS reviews
+FROM "EDW".fact_movie_analytics f
+INNER JOIN "EDW".dim_location dl ON dl.id_dim_location = f.id_dim_location
+INNER JOIN "EDW".dim_devices  dd ON dd.id_dim_devices  = f.id_dim_devices
+INNER JOIN "EDW".dim_os       dos ON dos.id_dim_os     = f.id_dim_os
+GROUP BY dl.location
+ORDER BY reviews
+LIMIT 10;
